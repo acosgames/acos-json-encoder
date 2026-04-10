c904f4c0ed721684eba6970bc2002c50ca805c0e
JoeTex <joelruiz2@gmail.com>
2026-04-09 23:10:11 -0500

    removing unused files

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { encode, decode, serialize, deserialize, serializeEX, deserializeEX, createDefaultDict } from "./encoder/encoder";
-export { registerProtocol, extendProtocol, revertProtocol, protoEncode, protoDecode, setDefaultDictionary, getProtocolSchema } from "./encoder/protocol";
-export { delta, merge, hidden, unhidden } from "./delta/delta";
-//# sourceMappingURL=index.d.ts.map
--- src/old/index.ts
+++
@@ -1,4 +0,0 @@
-export { encode, decode, serialize, deserialize, serializeEX, deserializeEX, createDefaultDict } from "../encoder/encoder";
-export { initProtocols, protoEncode, protoDecode } from "./protocol";
-export { registerProtocolWT, protoEncodeWT, protoDecodeWT, compileSchema, encodeNode, decodeNode, setStringDictionaryWT } from "./protocol-withtypes";
-export { delta, merge, hidden, unhidden } from "../delta/delta";
